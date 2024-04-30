2f5ea173ec3017590eb5b82588bbfb86a6602703
Sarah-0911 <sarah.tabet69@gmail.com>
2024-04-30 17:31:38 +0200

    add addWord and populateWordContainer js functions

--- Secret_Word/script.js
+++ Secret_Word/script.js
@@ -1,4 +1,33 @@
-const title = document.querySelector('h1');
+const newWords = document.querySelector('.new-words');
+const wordInput = newWords.querySelector('input[name="word"]');
+const wordContainer = document.querySelector('.word');
+const newWordContainer = document.querySelector('.add-word');
 
+  const wordToGuess = "Canuts";
+  console.log(wordContainer);
+  console.log(wordInput);
 
-const boxes = document.querySelectorAll('.box');
+  const addLetterClass = () => {
+    wordInput.classList.add('input-word');
+  }
+
+  const addWord = (e) => {
+  e.preventDefault();
+  const inputWord = wordInput.value;
+  addLetterClass();
+  populateWordContainer(inputWord, wordContainer);
+  populateWordContainer(inputWord, newWordContainer);
+  wordInput.value = '';
+};
+
+const populateWordContainer = (wordInput, wordContainer) => {
+  wordContainer.innerHTML = wordInput.split('').map((letter, index) => {
+    return `<div data-index="${index}" class="letter">${letter}</div>`
+  }).join('');
+};
+
+newWords.addEventListener('submit', addWord);
+wordInput.addEventListener('input', addLetterClass);
+newWordContainer.addEventListener('submit', addWord);
+
+// populateWordContainer(wordInput.value, wordContainer);
