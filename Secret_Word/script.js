faa6266ee6ae5a26060c1c69c4aae5eebdc910b8
Sarah-0911 <sarah.tabet69@gmail.com>
2024-05-02 15:34:47 +0200

    correctLetter function in progress

--- Secret_Word/script.js
+++ Secret_Word/script.js
@@ -2,17 +2,17 @@ const newWord = document.querySelector('.new-word');
 const wordInput = newWord.querySelector('input[name="word"]');
 const wordsContainer = document.querySelectorAll('.word');
 
-const rowOne = document.querySelector('.row-1');
-const rowTwo = document.querySelector('.row-2');
-const rowThree = document.querySelector('.row-3');
-const rowFour = document.querySelector('.row-4');
-const rowFive = document.querySelector('.row-5');
+// const rowOne = document.querySelector('.row-1');
+// const rowTwo = document.querySelector('.row-2');
+// const rowThree = document.querySelector('.row-3');
+// const rowFour = document.querySelector('.row-4');
+// const rowFive = document.querySelector('.row-5');
 
-const wordOne = rowOne.querySelectorAll('.letter');
-const wordTwo = rowTwo.querySelectorAll('.letter');
-const wordThree = rowThree.querySelectorAll('.letter');
-const wordFour = rowFour.querySelectorAll('.letter');
-const wordFive = rowFive.querySelectorAll('.letter');
+// const wordOne = rowOne.querySelectorAll('.letter');
+// const wordTwo = rowTwo.querySelectorAll('.letter');
+// const wordThree = rowThree.querySelectorAll('.letter');
+// const wordFour = rowFour.querySelectorAll('.letter');
+// const wordFive = rowFive.querySelectorAll('.letter');
 
 console.log(wordsContainer);
 
@@ -22,18 +22,23 @@ const customizeWord = () => {
   wordInput.classList.add('input-word');
 }
 
-const correctLetter = letter => {
-  letter.style.backgroundColor = 'red';
+const correctLetter = (letter, index) => {
+  if (wordToGuess.charAt(index) === letter.textContent) {
+    letter.style.backgroundColor = '#ff5a5a';
+  }
 }
 
-const wrongPosLetter = letter => {
-  letter.style.backgroundColor = 'yellow';
+const wrongPosLetter = word => {
+  if (word.charAt(letter) === wordToGuess.charAt(letter)) {
+    letter.style.backgroundColor = 'yellow';
+  }
 }
 
 // const wholeWord = (word) => {
 //   return Array.from(word).map(letter => letter.textContent).join('');
 // };
 // console.log(wholeWord(wordOne));
+
 
 const addWord = (e) => {
   e.preventDefault();
@@ -54,6 +59,7 @@ const addWord = (e) => {
     if (isRowEmpty) {
       inputWord.split('').forEach((letter, index) => {
         letters[index].textContent = letter;
+        correctLetter(letters[index], index);
       });
       if (inputWord === wordToGuess.toUpperCase()) {
         alert('You Won!')
